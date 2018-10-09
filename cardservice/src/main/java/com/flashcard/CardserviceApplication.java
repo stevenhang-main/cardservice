3fd07c93dd5efdfa1698403769441f8a5417d6fb
Steven Hang <stevenhang.main@gmail.com>
2018-10-09 13:38:12 -0400

    2nd initial

---
+++ cardservice/src/main/java/com/flashcard/CardserviceApplication.java
@@ -0,0 +1,12 @@
+package com.flashcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardserviceApplication.class, args);
+	}
+}
