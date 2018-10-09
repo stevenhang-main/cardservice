12c48a21faadba1fc7f5e8828ac2e1b8671d3063
David <djvelez@gmail.com>
2018-10-09 14:45:33 -0400

    added ratingandcomment bean to card-model

--- cardservice/src/main/java/com/cardservice/controller/CardController.java
+++ cardservice/src/main/java/com/cardservice/controller/CardController.java
@@ -1,8 +1,13 @@
 package com.cardservice.controller;
 
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cardservice.card.Card;
 import com.cardservice.services.CardService;
 
 @RestController
@@ -16,6 +21,11 @@ public class CardController {
 		for(Card c: cards) {
 			addLinksToCard(c);
 		}
+		return cards;
+	}
+
+	private void addLinksToCard(Card c) {
+		
 	}
 	
 	
